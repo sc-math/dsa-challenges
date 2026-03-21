d4bc114ee9015920831cfa950740f7d1b625f11c
Matheus Silva <scruz.math@gmail.com>
2026-03-21 18:35:22 -0300

    Daily(21/03) - 3643

--- src/leetcode/TestRunner.java
+++ src/leetcode/TestRunner.java
@@ -1,11 +1,18 @@
 package leetcode;
 
-import leetcode.matrix.P3212_CountSubmatricesEqualFreqXY;
+import leetcode.matrix.P3643_FlipSquareSubmatrixVertically;
+
+import java.util.Arrays;
 
 public class TestRunner {
     public static void main(String[] args) {
-        P3212_CountSubmatricesEqualFreqXY sol = new P3212_CountSubmatricesEqualFreqXY();
+        P3643_FlipSquareSubmatrixVertically sol = new P3643_FlipSquareSubmatrixVertically();
 
-        System.out.println(sol.numberOfSubmatrices(new char[][]{{'.', 'X'}, {'.', 'Y'}}));
+        System.out.println(Arrays.deepToString(sol.reverseSubmatrix(new int[][]{
+                        {1, 2, 3, 4},
+                        {5, 6, 7, 8},
+                        {9, 10, 11, 12},
+                        {13, 14, 15, 16}},
+                1, 0, 3)));
     }
 }
